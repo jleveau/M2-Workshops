ddae5d546eca6b9fa90460185c8eecfdbcc175c3
Florian Morel <flo.morel33@hotmail.fr>
2019-10-07 14:19:29 +0200

    update description of workshops

---
+++ src/app/inMemoryWorkshop.js
@@ -0,0 +1,61 @@
+const inMemoryWorkshop = [];
+
+
+function getWorkshopList() {
+  return new Promise((resolve) => {
+    resolve(inMemoryWorkshop);
+  });
+}
+
+function getWorkshopByName(name) {
+  return new Promise((resolve, reject) => {
+    if (!name) {
+      reject(new Error('name parameter is required'));
+    }
+    resolve(inMemoryWorkshop.find((workshop) => workshop.name === name));
+  });
+}
+
+function addWorkshop(name, description) {
+  return new Promise((resolve, reject) => {
+    if (!name) {
+      reject(new Error('Workshop name required'));
+    }
+    if (!description) {
+      reject(new Error('Workshop description required'));
+    }
+    inMemoryWorkshop.push({
+      name,
+      description,
+    });
+    resolve();
+  });
+}
+
+function removeWorkshopByName(name) {
+  return new Promise((resolve, reject) => {
+    reject(new Error('Not implemented'));
+  });
+}
+
+function updateWorkshop(name, description) {
+  return new Promise((resolve, reject) => {
+    if (!name && !description) {
+      reject(new Error('Workshop name and description needed'));
+    } else {
+      resolve(getWorkshopByName(name)
+        .then((workshop) => {
+          // eslint-disable-next-line no-param-reassign
+          workshop.description = description;
+        }));
+    }
+  });
+}
+
+module.exports = {
+  getWorkshopList,
+  getWorkshopByName,
+  addWorkshop,
+  removeWorkshopByName,
+  updateWorkshop,
+};
