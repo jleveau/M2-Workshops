5b965443e9ecee76a097ec1bbdb413ef5905e71c
Willou <wil-etan@hotmail.fr>
2020-10-07 16:01:20 +0200

    TD en cours...

--- js/inMemoryWorkshop.js
+++ js/inMemoryWorkshop.js
@@ -1,53 +1,60 @@
-inMemoryWorkshop = []
+let inMemoryWorkshop = [];
 
 
 function getWorkshopList() {
-    return new Promise((resolve, ) => {
-        resolve(inMemoryWorkshop)
-    })
+	return new Promise((resolve, ) => {
+		resolve(inMemoryWorkshop);
+	});
 }
 
 function getWorkshopByName(name) {
-    return new Promise((resolve, reject) => {
-        if (!name) {
-            reject(new Error("name parameter is required"))
-        }
-        resolve(inMemoryWorkshop.find(workshop => workshop.name === workshop))
-    })
+	return new Promise((resolve, reject) => {
+		if (!name) {
+			reject(new Error("name parameter is required"));
+		}
+		resolve(inMemoryWorkshop.find(workshop => workshop.name === workshop));
+	});
 }
 
 function addWorkshop(name, description) {
-    return new Promise((resolve, reject) => {
-        if (!name) {
-            reject(new Error("Workshop name required"))
-        }
-        if (!description) {
-            reject(new Error("Workshop description required"))
-        }
-        inMemoryWorkshop.push({
-            name,
-            description
-        })
-        resolve()
-    })
+	return new Promise((resolve, reject) => {
+		if (!name) {
+			reject(new Error("Workshop name required"));
+		}
+		if (!description) {
+			reject(new Error("Workshop description required"));
+		}
+		inMemoryWorkshop.push({
+			name,
+			description
+		});
+		resolve();
+	});
 }
 
 function removeWorkshopByName(name) {
-    return new Promise((resolve, reject) => {
-        reject(new Error("Not implemented"))
-    })
+	console.log("ok");
+	return new Promise((resolve, reject) => {
+		if(!name){
+			reject(new Error("Workshop name required"));
+		}
+		console.log(inMemoryWorkshop);
+		let i = inMemoryWorkshop.findIndex(name);
+		inMemoryWorkshop.splice(i, 1);
+		resolve();
+	});
 }
 
 function updateWorkshop(name) {
-    return new Promise((resolve, reject) => {
-        reject(new Error("Not implemented"))
-    })
+	return new Promise((resolve, reject) => {
+		reject(new Error("Not implemented"));
+	});
 }
 
 module.exports = {
-    getWorkshopList,
-    getWorkshopByName,
-    addWorkshop,
-    removeWorkshopByName,
-    updateWorkshop
-}
+	getWorkshopList,
+	getWorkshopByName,
+	addWorkshop,
+	removeWorkshopByName,
+	updateWorkshop
+};
--- js/index.js
+++ js/index.js
@@ -1,64 +1,68 @@
+/* eslint-disable no-undef */
 
-const express = require('express')
-const app = express()
-const InMemoryWorkshop = require("./inMemoryWorkshop")
-const path = require("path")
-const ejs = require('ejs')
-var bodyParser = require('body-parser')
+const express = require("express");
+const app = express();
+const InMemoryWorkshop = require("./inMemoryWorkshop");
+const path = require("path");
+const ejs = require("ejs");
+var bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '..', '/ejs'));
-app.use(express.static(path.join(__dirname , '..', 'css')));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "..", "/ejs"));
+app.use(express.static(path.join(__dirname , "..", "css")));
 
 
-app.get('/', function (req, res) {
-    InMemoryWorkshop.getWorkshopList()
-    .then(workshops => {
-        res.render("index", {
-            workshops: workshops
-        })
-    })
-})
+app.get("/", function (req, res) {
+	InMemoryWorkshop.getWorkshopList()
+		.then(workshops => {
+			res.render("pages/index", {
+				workshops: workshops
+			});
+		});
+});
 
-app.get('/workshop', function (req, res) {
-    console.log("get")
-    res.render('workshop')
-})
+app.get("/workshop", function (req, res) {
+	console.log("get");
+	res.render("pages/workshop");
+});
 
-app.post('/workshop', function (req, res) {
-    const name = req.body.name
-    const description = req.body.description
-    InMemoryWorkshop.addWorkshop(name, description).then(() => {
-        InMemoryWorkshop.getWorkshopList()
-        .then(workshops => {
-            res.render("index", {
-                workshops: workshops
-            })
-        })
-    })
-    .catch(e =>res.send(e.message))
-})
+app.post("/workshop", function (req, res) {
+	const name = req.body.name;
+	const description = req.body.description;
+	InMemoryWorkshop.addWorkshop(name, description).then(() => {
+		InMemoryWorkshop.getWorkshopList()
+			.then(workshops => {
+				res.render("pages/index", {
+					workshops: workshops
+				});
+			});
+	})
+		.catch(e =>res.send(e.message));
+});
 
-app.get('/workshop/:name', function (req, res) {
-    const workshopName = req.params.name
-    InMemoryWorkshop.getWorkshopByName(workshopName)
-    .then(workshop => {
-        res.render('ejs/workshop', workshop)
-    })
-    .catch(e =>ejs.send(e.message))
-})
+app.get("/workshop/:name", function (req, res) {
+	const workshopName = req.params.name;
+	InMemoryWorkshop.getWorkshopByName(workshopName)
+		.then(workshop => {
+			res.render("ejs/pages/workshop", workshop);
+		})
+		.catch(e =>ejs.send(e.message));
+});
 
-app.post('/remove-workshop', function (req, res) {
-    res.status(500).send("TODO")
-})
+app.post("/remove-workshop", function (req, res) {
+	const name = req.body.name;
+	InMemoryWorkshop.removeWorkshopByName(name);
+	
 
-app.post('/update-workshop', function(req, res) {
-    res.status(500).send("TODO")
-})
+});
+
+app.post("/update-workshop", function(req, res) {
+	res.status(500).send("TODO");
+});
 
 app.listen(3000, function () {
-  console.log('Workshop app listening on port 3000!')
-})
+	console.log("Workshop app listening on port 3000!");
+});
