9233de91ca29740ea23eb0898eb29898e091d1dd
Thimotee Brocard <tbrocard@emi.u-bordeaux.fr>
2020-10-07 14:49:14 +0200

    Debut TD4

--- js/inMemoryWorkshop.js
+++ js/inMemoryWorkshop.js
@@ -1,47 +1,47 @@
-inMemoryWorkshop = []
+inMemoryWorkshop = [];
 
 
 function getWorkshopList() {
     return new Promise((resolve, ) => {
-        resolve(inMemoryWorkshop)
-    })
+        resolve(inMemoryWorkshop);
+    });
 }
 
 function getWorkshopByName(name) {
     return new Promise((resolve, reject) => {
         if (!name) {
-            reject(new Error("name parameter is required"))
+            reject(new Error("name parameter is required"));
         }
-        resolve(inMemoryWorkshop.find(workshop => workshop.name === workshop))
-    })
+        resolve(inMemoryWorkshop.find(workshop => workshop.name === workshop));
+    });
 }
 
 function addWorkshop(name, description) {
     return new Promise((resolve, reject) => {
         if (!name) {
-            reject(new Error("Workshop name required"))
+            reject(new Error("Workshop name required"));
         }
         if (!description) {
-            reject(new Error("Workshop description required"))
+            reject(new Error("Workshop description required"));
         }
         inMemoryWorkshop.push({
             name,
             description
-        })
-        resolve()
-    })
+        });
+        resolve();
+    });
 }
 
 function removeWorkshopByName(name) {
     return new Promise((resolve, reject) => {
-        reject(new Error("Not implemented"))
-    })
+        reject(new Error("Not implemented"));
+    });
 }
 
 function updateWorkshop(name) {
     return new Promise((resolve, reject) => {
-        reject(new Error("Not implemented"))
-    })
+        reject(new Error("Not implemented"));
+    });
 }
 
 module.exports = {
--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 
-const express = require('express')
-const app = express()
-const InMemoryWorkshop = require("./inMemoryWorkshop")
-const path = require("path")
-const ejs = require('ejs')
-var bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const InMemoryWorkshop = require("./inMemoryWorkshop");
+const path = require("path");
+const ejs = require('ejs');
+var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -19,46 +19,46 @@ app.get('/', function (req, res) {
     .then(workshops => {
         res.render("index", {
             workshops: workshops
-        })
-    })
-})
+        });
+    });
+});
 
 app.get('/workshop', function (req, res) {
-    console.log("get")
-    res.render('workshop')
-})
+    console.log("get");
+    res.render('workshop');
+});
 
 app.post('/workshop', function (req, res) {
-    const name = req.body.name
-    const description = req.body.description
+    const name = req.body.name;
+    const description = req.body.description;
     InMemoryWorkshop.addWorkshop(name, description).then(() => {
         InMemoryWorkshop.getWorkshopList()
         .then(workshops => {
             res.render("index", {
                 workshops: workshops
-            })
-        })
+            });
+        });
     })
-    .catch(e =>res.send(e.message))
-})
+    .catch(e =>res.send(e.message));
+});
 
 app.get('/workshop/:name', function (req, res) {
-    const workshopName = req.params.name
+    const workshopName = req.params.name;
     InMemoryWorkshop.getWorkshopByName(workshopName)
     .then(workshop => {
-        res.render('ejs/workshop', workshop)
+        res.render('ejs/workshop', workshop);
     })
-    .catch(e =>ejs.send(e.message))
-})
+    .catch(e =>ejs.send(e.message));
+});
 
 app.post('/remove-workshop', function (req, res) {
-    res.status(500).send("TODO")
-})
+    res.status(500).send("TODO");
+});
 
 app.post('/update-workshop', function(req, res) {
-    res.status(500).send("TODO")
-})
+    res.status(500).send("TODO");
+});
 
 app.listen(3000, function () {
-  console.log('Workshop app listening on port 3000!')
-})
+  console.log('Workshop app listening on port 3000!');
+});
