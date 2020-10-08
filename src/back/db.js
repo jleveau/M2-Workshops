cba05c7357a43237f488c63006384d53e6847301
N3ROO <swellchaser33@gmail.com>
2020-10-08 20:05:31 +0200

    fixed all eslint issues (closes #2)

--- src/back/db.js
+++ src/back/db.js
@@ -1,53 +1,77 @@
-db = []
+db = [];
 
-
+/**
+ * TODO
+ * @return {Promise}
+ */
 function getWorkshopList() {
-    return new Promise((resolve, ) => {
-        resolve(db)
-    })
+  return new Promise((resolve ) => {
+    resolve(db);
+  });
 }
 
+/**
+ * TODO
+ * @param {String} name
+ * @return {Promise}
+ */
 function getWorkshopByName(name) {
-    return new Promise((resolve, reject) => {
-        if (!name) {
-            reject(new Error("name parameter is required"))
-        }
-        resolve(db.find(workshop => workshop.name === workshop))
-    })
+  return new Promise((resolve, reject) => {
+    if (!name) {
+      reject(new Error('name parameter is required'));
+    }
+    resolve(db.find((workshop) => workshop.name === workshop));
+  });
 }
 
+/**
+ * TODO
+ * @param {String} name
+ * @param {String} description
+ * @return {Promise}
+ */
 function addWorkshop(name, description) {
-    return new Promise((resolve, reject) => {
-        if (!name) {
-            reject(new Error("Workshop name required"))
-        }
-        if (!description) {
-            reject(new Error("Workshop description required"))
-        }
-        db.push({
-            name,
-            description
-        })
-        resolve()
-    })
+  return new Promise((resolve, reject) => {
+    if (!name) {
+      reject(new Error('Workshop name required'));
+    }
+    if (!description) {
+      reject(new Error('Workshop description required'));
+    }
+    db.push({
+      name,
+      description,
+    });
+    resolve();
+  });
 }
 
+/**
+ * TODO
+ * @param {String} name
+ * @return {Promise}
+ */
 function removeWorkshopByName(name) {
-    return new Promise((resolve, reject) => {
-        reject(new Error("Not implemented"))
-    })
+  return new Promise((resolve, reject) => {
+    reject(new Error('Not implemented'));
+  });
 }
 
+/**
+ * TODO
+ * @param {String} name
+ * @return {Promise}
+ */
 function updateWorkshop(name) {
-    return new Promise((resolve, reject) => {
-        reject(new Error("Not implemented"))
-    })
+  return new Promise((resolve, reject) => {
+    reject(new Error('Not implemented'));
+  });
 }
 
 module.exports = {
-    getWorkshopList,
-    getWorkshopByName,
-    addWorkshop,
-    removeWorkshopByName,
-    updateWorkshop
-}
+  getWorkshopList,
+  getWorkshopByName,
+  addWorkshop,
+  removeWorkshopByName,
+  updateWorkshop,
+};
--- src/back/run.js
+++ src/back/run.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const api = require('./api')
-var bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const path = require('path');
+const api = require('./api');
+const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '..', 'front/templates'));
-app.use(express.static(path.join(__dirname , '..', 'front/style')));
+app.use(express.static(path.join(__dirname, '..', 'front/style')));
 
 api.start(app);
 
-app.listen(3000, function () {
-    console.log('Workshop app listening on port 3000!')
-})
+app.listen(3000, function() {
+  console.log('Workshop app listening on port 3000!');
+});
