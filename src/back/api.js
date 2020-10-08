8d18e04090f34d96c6085e55b0b718a975236573
N3ROO <swellchaser33@gmail.com>
2020-10-08 20:28:50 +0200

    renamed endpoints, closes #5

--- src/back/api.js
+++ src/back/api.js
@@ -5,6 +5,10 @@ const db = require('./db');
  * @param {Express} app
  */
 function start(app) {
+  // ----------------
+  // "HTML" ENDPOINTS
+  // ----------------
+
   app.get('/', function(req, res) {
     db.getWorkshopList()
         .then((workshops) => {
@@ -19,20 +23,6 @@ function start(app) {
     res.render('workshop');
   });
 
-  app.post('/workshop', function(req, res) {
-    const name = req.body.name;
-    const description = req.body.description;
-    db.addWorkshop(name, description).then(() => {
-      db.getWorkshopList()
-          .then((workshops) => {
-            res.render('index', {
-              workshops: workshops,
-            });
-          });
-    })
-        .catch((e) =>res.send(e.message));
-  });
-
   app.get('/workshop/:name', function(req, res) {
     const workshopName = req.params.name;
     db.getWorkshopByName(workshopName)
@@ -42,11 +32,23 @@ function start(app) {
         .catch((e) =>ejs.send(e.message));
   });
 
-  app.post('/remove-workshop', function(req, res) {
+  // ----------------
+  // API ENDPOINTS
+  // ----------------
+
+  app.post('/api/workshop', function(req, res) {
+    const name = req.body.name;
+    const description = req.body.description;
+    db.addWorkshop(name, description)
+        .then(() => res.redirect('/'))
+        .catch((e) => res.send(e.message));
+  });
+
+  app.post('/api/remove-workshop', function(req, res) {
     res.status(500).send('TODO');
   });
 
-  app.post('/update-workshop', function(req, res) {
+  app.post('/api/update-workshop', function(req, res) {
     res.status(500).send('TODO');
   });
 }
