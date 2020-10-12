9866b94fa00f98b6124a327db587970a30c9b1e7
root <root@LAPTOP-78PI252I.localdomain>
2020-10-12 17:50:56 +0200

    fix eslint errors

--- js/inMemoryWorkshop.js
+++ js/inMemoryWorkshop.js
@@ -1,71 +1,68 @@
-inMemoryWorkshop = []
+const inMemoryWorkshop = []
 
-
-function getWorkshopList() {
-    return new Promise((resolve,) => {
-        resolve(inMemoryWorkshop)
-    })
+function getWorkshopList () {
+  return new Promise((resolve) => {
+    resolve(inMemoryWorkshop)
+  })
 }
 
-function getWorkshopByName(name) {
-    return new Promise((resolve, reject) => {
-        if (!name) {
-            reject(new Error("name parameter is required"))
-        }
-        resolve(inMemoryWorkshop.find(workshop => workshop.name === workshop))
-    })
+function getWorkshopByName (name) {
+  return new Promise((resolve, reject) => {
+    if (!name) {
+      reject(new Error('name parameter is required'))
+    }
+    resolve(inMemoryWorkshop.find(workshop => workshop.name === workshop))
+  })
 }
 
-function addWorkshop(name, description) {
-    return new Promise((resolve, reject) => {
-        if (!name) {
-            reject(new Error("Workshop name required"))
-        }
-        if (!description) {
-            reject(new Error("Workshop description required"))
-        }
-        inMemoryWorkshop.push({
-            name,
-            description
-        })
-        resolve()
+function addWorkshop (name, description) {
+  return new Promise((resolve, reject) => {
+    if (!name) {
+      reject(new Error('Workshop name required'))
+    }
+    if (!description) {
+      reject(new Error('Workshop description required'))
+    }
+    inMemoryWorkshop.push({
+      name,
+      description
     })
+    resolve()
+  })
 }
 
-function removeWorkshopByName(name) {
-    return new Promise((resolve, reject) => {
-        
-        var obj = JSON.parse(JSON.stringify(inMemoryWorkshop))
-        for (var i = 0; i < inMemoryWorkshop.length; i++) {
-            console.log("obj"+obj[i].name)
-            console.log(name)
-        if (obj[i].name == name) {
-            console.log("here")
-            inMemoryWorkshop.splice(i,1)
-            }
-        }
-        resolve()
-    })
+function removeWorkshopByName (name) {
+  return new Promise((resolve, reject) => {
+    var obj = JSON.parse(JSON.stringify(inMemoryWorkshop))
+    for (var i = 0; i < inMemoryWorkshop.length; i++) {
+      console.log('obj' + obj[i].name)
+      console.log(name)
+      if (obj[i].name == name) {
+        console.log('here')
+        inMemoryWorkshop.splice(i, 1)
+      }
+    }
+    resolve()
+  })
 }
 
-function updateWorkshop(name, oldName) {
-    return new Promise((resolve, reject) => {
-        var obj = JSON.parse(JSON.stringify(inMemoryWorkshop))
-        for (var i = 0; i < inMemoryWorkshop.length; i++) {
-            if (obj[i].name == oldName) {
-                obj[i].name = name
-                inMemoryWorkshop[i] = obj[i]
-            }
-        }
-        resolve()
-
-    })
+function updateWorkshop (name, oldName) {
+  return new Promise((resolve, reject) => {
+    var obj = JSON.parse(JSON.stringify(inMemoryWorkshop))
+    for (var i = 0; i < inMemoryWorkshop.length; i++) {
+      if (obj[i].name == oldName) {
+        obj[i].name = name
+        inMemoryWorkshop[i] = obj[i]
+      }
+    }
+    resolve()
+  })
 }
 
 module.exports = {
-    getWorkshopList,
-    getWorkshopByName,
-    addWorkshop,
-    removeWorkshopByName,
-    updateWorkshop
+  getWorkshopList,
+  getWorkshopByName,
+  addWorkshop,
+  removeWorkshopByName,
+  updateWorkshop
 }
