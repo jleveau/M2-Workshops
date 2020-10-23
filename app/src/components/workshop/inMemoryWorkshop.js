5ab0fe5537d184ba10508744ff0a5d882105476b
vahnrr <vahnrr@pm.me>
2020-10-23 17:57:49 +0200

    Q2 Définition et application des conventions.

--- app/src/components/workshop/inMemoryWorkshop.js
+++ app/src/components/workshop/inMemoryWorkshop.js
@@ -1,58 +1,58 @@
-let inMemoryWorkshop;
+let inMemoryWorkshop
 
-function init() {
-    inMemoryWorkshop = [];
-    return Promise.resolve();
+function init () {
+  inMemoryWorkshop = []
+  return Promise.resolve()
 }
 
-function getWorkshopList() {
-    return new Promise((resolve, ) => {
-        resolve(inMemoryWorkshop)
-    })
+function getWorkshopList () {
+  return new Promise((resolve) => {
+    resolve(inMemoryWorkshop)
+  })
 }
 
-function getWorkshopByName(name) {
-    return new Promise((resolve, reject) => {
-        if (!name) {
-            reject(new Error("name parameter is required"))
-        }
-        resolve(inMemoryWorkshop.find(workshop => workshop.name === workshop))
-    })
+function getWorkshopByName (name) {
+  return new Promise((resolve, reject) => {
+    if (!name) {
+      reject(new Error("name parameter is required"))
+    }
+    resolve(inMemoryWorkshop.find(workshop => workshop.name === workshop))
+  })
 }
 
-function addWorkshop(name, description) {
-    return new Promise((resolve, reject) => {
-        if (!name) {
-            reject(new Error("Workshop name required"))
-        }
-        if (!description) {
-            reject(new Error("Workshop description required"))
-        }
-        inMemoryWorkshop.push({
-            name,
-            description
-        })
-        resolve()
+function addWorkshop (name, description) {
+  return new Promise((resolve, reject) => {
+    if (!name) {
+      reject(new Error("Workshop name required"))
+    }
+    if (!description) {
+      reject(new Error("Workshop description required"))
+    }
+    inMemoryWorkshop.push({
+      name,
+      description
     })
+    resolve()
+  })
 }
 
-function removeWorkshopByName(name) {
-    return new Promise((resolve, reject) => {
-        reject(new Error("Not implemented"))
-    })
+function removeWorkshopByName (name) {
+  return new Promise((resolve, reject) => {
+    reject(new Error("Not implemented"))
+  })
 }
 
-function updateWorkshop(name, description) {
-    return new Promise((resolve, reject) => {
-        reject(new Error("Not implemented"))
-    })
+function updateWorkshop (name, description) {
+  return new Promise((resolve, reject) => {
+    reject(new Error("Not implemented"))
+  })
 }
 
 module.exports = {
-    init,
-    getWorkshopList,
-    getWorkshopByName,
-    addWorkshop,
-    removeWorkshopByName,
-    updateWorkshop
+  init,
+  getWorkshopList,
+  getWorkshopByName,
+  addWorkshop,
+  removeWorkshopByName,
+  updateWorkshop
 }
